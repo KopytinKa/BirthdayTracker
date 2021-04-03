3b9b07d4339be268173ece7cde61963e9951e7bb
Kirill Kopytin <kopytin-ka@mail.ru>
2021-04-03 17:01:54 +0300

    настроен визуальный интерфейс для приложения

---
+++ BirthdayTracker/BirthdayTracker/Birthday.swift
@@ -0,0 +1,8 @@
+//
+//  Birthday.swift
+//  BirthdayTracker
+//
+//  Created by Кирилл Копытин on 03.04.2021.
+//
+
+import Foundation
---
+++ BirthdayTracker/BirthdayTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Кирилл Копытин on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
